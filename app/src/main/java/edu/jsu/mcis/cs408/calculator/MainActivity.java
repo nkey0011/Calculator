94d4a054690b9d5af782ac1e3c131489298dda89
nkey <nkey@stu.jsu.edu>
2022-02-22 13:58:55 -0600

    added part 2 initial code

--- app/src/main/java/edu/jsu/mcis/cs408/calculator/MainActivity.java
+++ app/src/main/java/edu/jsu/mcis/cs408/calculator/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.jsu.mcis.cs408.calculator;
 
+// controller class
+
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
@@ -9,11 +11,37 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    class CalculatorClickHandler implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            String tag = ((Button) view).getTag().toString();
+            Toast toast = Toast.makeText(binding.getRoot().getContext(), tag, Toast.LENGTH_SHORT);
+            toast.show();
+            // INSERT EVENT HANDLING CODE HERE
+        }
+
+    }
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        CalculatorClickHandler click = new CalculatorClickHandler();
+        ConstraintLayout layout = binding.layout;
+
+        for (int i = 0; i < layout.getChildCount(); ++i) {
+            View child = layout.getChildAt(i);
+            if(child instanceof Button) {
+                child.setOnClickListener(click);
+            }
+        }
+
+
     }
 }
